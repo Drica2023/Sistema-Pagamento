80e2d6c9156543798441698d56cf4ba7b8646bcd
Adriana <adrianamedeiros.dev@outlook.com>
2024-02-24 15:03:19 -0300

    adicionando as Annotation no Sistema de Pagamento

--- SpringBoot/sistema/src/main/java/br/com/pagamento/sistema/SistemaApplication.java
+++ SpringBoot/sistema/src/main/java/br/com/pagamento/sistema/SistemaApplication.java
@@ -3,10 +3,14 @@ package br.com.pagamento.sistema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Annotation => Indica que é um projeto SpringBoot
 @SpringBootApplication
 public class SistemaApplication {
 
 	public static void main(String[] args) {
+		// Ele cria um contexto que é uma aplicação Spring
+		// Varredura, das classes antes de iniciar
+		// Inicia a Aplicação
 		SpringApplication.run(SistemaApplication.class, args);
 	}
 
